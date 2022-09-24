e183db432690b979245cfd878e882c3cda08f2ce
Guilherme <gwvsantos@hotmail.com>
2022-09-24 17:09:55 -0300

    Jogo 100% finalizado

--- JogoDef.py
+++ JogoDef.py
@@ -48,42 +48,76 @@ def tipo_face():
     cerebro = 0
     passo = 0
     tiro = 0
+    verde_passo = 0
+    amarelo_passo = 0
+    vermelho_passo = 0
     for d in range(cores[0]):
         face = random.randint(0, 5)
-        print(face)
         if face == 0 or face == 2 or face == 5:
             cerebro += 1
         elif face == 1 or face == 4:
             passo += 1
+            verde_passo += 1
         else:
             tiro += 1
     for d in range(cores[1]):
         face = random.randint(0, 5)
-        print(face)
         if face == 2 or face == 5:
             cerebro += 1
         elif face == 1 or face == 4:
             passo += 1
+            amarelo_passo += 1
         else:
             tiro += 1
     for d in range(cores[2]):
         face = random.randint(0, 5)
-        print(face)
         if face == 3:
             cerebro += 1
         elif face == 1 or face == 4:
             passo += 1
+            vermelho_passo += 1
         else:
             tiro += 1
     contadores[7] += cerebro
     contadores[8] += tiro
-    return [cerebro, passo, tiro, contadores[7], contadores[8]]
+    return [cerebro, passo, tiro, contadores[7], contadores[8], verde_passo, amarelo_passo, vermelho_passo]
+
+
+def retorno_passo():
+    retorno_verde = 0
+    retorno_amarelo = 0
+    retorno_vermelho = 0
+    if acao[1] != 0:
+        print(f'Os {acao[1]} dados de PASSO voltaram ao copo!')
+        for k in range(acao[1]):
+            if acao[5] != 0:
+                for t in range(acao[5]):
+                    for v in sorteados:
+                        if v == 1 or v == 2 or v == 3 or v == 4 or v == 5 or v == 6:
+                            sorteados.remove(v)
+                            dados.append(v)
+                            retorno_verde += 1
+            if acao[6] != 0:
+                for t in range(acao[6]):
+                    for v in sorteados:
+                        if v == 7 or v == 8 or v == 9 or v == 10:
+                            sorteados.remove(v)
+                            dados.append(v)
+                            retorno_amarelo += 1
+            if acao[7] != 0:
+                for t in range(acao[7]):
+                    for v in sorteados:
+                        if v == 11 or v == 12 or v == 13:
+                            sorteados.remove(v)
+                            dados.append(v)
+                            retorno_vermelho += 1
+    return [retorno_verde, retorno_amarelo, retorno_vermelho]
 
 
 def condicoes():
     roda = 0
     if lista_cerebros[cont] >= 13:
-        print(f'{lista_nomes[cont]} VOCÊ VENCEU O JOGO!!!')
+        print(f'PARABÉNS {lista_nomes[cont]} VOCÊ VENCEU O JOGO!!!')
         roda = 3
     elif len(dados) <= 2:
         print('Seus dados acabaram!')
@@ -128,10 +162,13 @@ for z in range(0, n_j):
 
 while True:
     for cont in range(0, n_j):
-        print(f'É a vez do jogador {lista_nomes[cont]}')
+        print(f'\nÉ a vez do jogador {lista_nomes[cont]}')
         contadores = jogadores()
         dados = todos_dados()
         cerebro_total = lista_cerebros[cont]
+        verde_copo = 6
+        amarelo_copo = 4
+        vermelho_copo = 3
         while True:
             sorteados = (random.sample(dados, 3))
             for n in sorteados:
@@ -151,9 +188,17 @@ while True:
             if acao[3] != 0:
                 print(f'Você avançou {acao[0]} casas!')
             else:
-                print(f'Você não avançou casas!')
+                print(f'Você não avançou casas nesta rodada!')
             print(f'{lista_nomes[cont]} você já avançou {lista_cerebros[cont]} casas!')
             print(f'{lista_nomes[cont]} você está com {acao[4]} tiros!!')
+            retorno = retorno_passo()
+            verde_copo = verde_copo - cores[3] + retorno[0]
+            amarelo_copo = amarelo_copo - cores[4] + retorno[1]
+            vermelho_copo = vermelho_copo - cores[5] + retorno[2]
+            print(f'Sobraram {verde_copo + amarelo_copo + vermelho_copo} dados: ')
+            print(f'    -> {verde_copo} dados VERDES '
+                  f'\n    -> {amarelo_copo} dados AMARELOS '
+                  f'\n    -> {vermelho_copo} dados VERMELHOS')
             cond = condicoes()
             if cond[0] == 1:
                 break
